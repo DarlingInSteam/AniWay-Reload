6409b85a884acfbd6ce61a837557a5d36b819dd8
Darling <92879479+DarlingInSteam@users.noreply.github.com>
2025-10-07 01:58:58 +0700

    feat(integration): add lazy initialization for AutoParsingService in MelonIntegrationService

--- MangaService/src/main/java/shadowshift/studio/mangaservice/service/MelonIntegrationService.java
+++ MangaService/src/main/java/shadowshift/studio/mangaservice/service/MelonIntegrationService.java
@@ -4,6 +4,7 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Lazy;
 import org.springframework.core.io.ByteArrayResource;
 import org.springframework.http.*;
 import org.springframework.scheduling.annotation.Async;
@@ -52,6 +53,7 @@ public class MelonIntegrationService {
     private TagService tagService;
 
     @Autowired
+    @Lazy
     private AutoParsingService autoParsingService;
 
     /**
