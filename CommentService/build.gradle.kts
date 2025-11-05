46758b6864f38507262b895d0f1edf0fd8982e0e
Darling <92879479+DarlingInSteam@users.noreply.github.com>
2025-11-05 23:36:18 +0700

    Add Prometheus metrics support and update actuator configurations

--- CommentService/build.gradle.kts
+++ CommentService/build.gradle.kts
@@ -34,6 +34,7 @@ dependencies {
     implementation("org.springframework.boot:spring-boot-starter-validation")
     implementation("org.springframework.boot:spring-boot-starter-security")
     implementation("org.springframework.boot:spring-boot-starter-actuator")
+    implementation("io.micrometer:micrometer-registry-prometheus")
     implementation("org.springframework.boot:spring-boot-starter-amqp")
     implementation("org.springframework.retry:spring-retry")
     
